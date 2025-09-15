58de08e0866cb9ea882d7facef47de6ae8d0af81
Ali Syed <alisyed0206@gmail.com>
2025-09-15 07:03:02 -0400

    Initial Commit

---
+++ BarCharts/BarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartsApp.swift
+//  BarCharts
+//
+//  Created by Ali Syed on 2025-09-15.
+//
+
+import SwiftUI
+
+@main
+struct BarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarChartsTests/BarChartsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartsTests.swift
+//  BarChartsTests
+//
+//  Created by Ali Syed on 2025-09-15.
+//
+
+import Testing
+@testable import BarCharts
+
+struct BarChartsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
